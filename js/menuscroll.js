54439deac43a7a4dcb9857be0179f51e6a45cba8
PhatJack <jackvuabongro@gmail.com>
2022-10-25 21:43:34 +0700

    scroll to top

--- js/menuscroll.js
+++ js/menuscroll.js
@@ -1,13 +1,17 @@
 const body = document.body;
 const nav = document.querySelector(".container");
+const scrollHome = document.querySelector(".back-to-top");
 window.addEventListener("scroll", () => {
   const currentscroll = window.pageYOffset;
+  console.log(currentscroll);
   if (currentscroll > 120) {
     // console.log(currentscroll);
     nav.classList.add("active");
-  } else {
-    // console.log(currentscroll);
+    scrollHome.classList.add("active");
 
+  } else {
     nav.classList.remove("active");
+    scrollHome.classList.remove("active");
+
   }
 });
