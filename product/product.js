63fb1827a857f65bbc161c2f6107dca557d977f0
Bui Ngoc Thuc <buingocthuc1512@gmail.com>
2022-11-24 10:57:50 +0700

    add product

---
+++ product/product.js
@@ -0,0 +1,340 @@
+var productData = [
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL ASTRINGENT FOAM",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/AstringentFoamMousse_1296x-768x767.jpg.webp",
+    review:
+      "You’ve just run a razor over your face. Maybe you owe it a little something extra? Reuzel Astringent Foam is non-drying and oil-free, so it’ll give your skin the firm, smooth look you’re after without removing essential moisture.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL SHAVE CREAM",
+    price: "320000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/ShaveCream-1-768x768.jpg.webp",
+    review:
+      "In the old days, a town’s barbers also did its bloodletting. Let’s just say that isn’t how we do things anymore. Reuzel Shave Cream is a highly concentrated formula. Our T4 Tonic Blend combos with quinoa, aloe vera and sorbitol to give you a close shave, no nicking necessary.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD FOAM",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/BeardFoam-2-1-768x768.jpg.webp",
+    review:
+      "Keep your beard under control with our leave-in Reuzel Beard Foam. The same T4 Tonic Blend that makes our hair care products special will do wonders here, as well. Just a bit will reduce beardruff, tame rogue hairs, add fullness and knock out that funky beard smell.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD WASH",
+    price: "490000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/BeardWash_1296x-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL SOLID COLOGNE WOOD & SPICE",
+    price: "350000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/SolidCologne-1-768x768.jpg.webp",
+    review:
+      "The Wood & Spice scent you used to but now available in a solid cologne. Direction of use: Scrape a little out with your finger nail and warm it between you fingers. Apply where desired.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD BALM",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/BeardBalm_2-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD BALM WOOD & SPICE",
+    price: "350000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/BeardBalm-1-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL ‘THE STACHE’ MOUSTACHE WAX",
+    price: "350000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2021/06/Mustache-Wax-copy-768x768.jpg.webp",
+    review:
+      "Reuzel Stache Wax is a beeswax based formula that provides a firm hold with a natural finish. In honor of our old friend Kurtis “Stache” Lawrence, a portion of the proceeds from the sale of Stache Wax is used for scholarships at The Old School Barber Academy in Rotterdam.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD FOAM WOOD & SPICE",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/BeardFoam-1-1-768x768.jpg.webp",
+    review:
+      "Reuzel Wood & Spice Beard Foam features a brand-new fragrance that’s a bit of a slap in the face. (And who doesn’t like that?) Massage it in, and you’ll find it controls wayward hairs and conditions the skin underneath.",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL BEARD SERUM",
+    price: "650000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/BeardSerum_1296x-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL REFRESH NO RINSE BEARD WASH",
+    price: "460000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/Refresh-200ml_1296x-768x767.jpg.webp",
+    review: "",
+  },
+  {
+    category: "BEARD & SHAVE",
+    name: "REUZEL AFTERSHAVE",
+    price: "320000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/Aftershave-2-1-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "BODY SOAP BAR",
+    price: "470000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/06/REU-087-1-768x768.jpg.webp",
+    review:
+      "REUZEL® Body Bar is a hydrating cleansing bar for the entire body. REUZEL® Body Bar includes Quartz for natural exfoliation, and the essence of Tea Tree leaves your skin feeling refreshed, clean and hydrated. Scented with refreshing Spearmint and hints of Tea Tree.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "REUZEL 3-IN-1 TEA TREE ",
+    price: "530000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/3in1TeaTree-1-768x768.jpg.webp",
+    review:
+      "On the go? Or maybe just trying to keep life simple? Keep Reuzel 3-in-1 Tea Tree Shampoo, Conditioner & Body Wash on hand. It’s a one-stop shop for cleaning and conditioning. It belongs in every man’s dopp kit and gym bag.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "REUZEL DAILY SHAMPOO",
+    price: "860000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/DailyShampoo_1296x-768x768.jpg.webp",
+    review:
+      "Want to start every day fresh? Here you go, friend. Reuzel Daily Shampoo cleans without stripping. It smells great. And our T4 tonic blend of witch hazel, nettle leaf, rosemary and horsetail root extracts will leave you with a stronger, shinier head of hair.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "REUZEL HAIR TONIC",
+    price: "620000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/HairTonic-1-768x768.jpg.webp",
+    review:
+      "Step back in time into a classic barbershop, where guys wanted to clean their scalp without stripping the grease from their hair. Reuzel Hair Tonic is just the thing. It’s a multipurpose tool with a classic masculine smell.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "SPRAY GROOMING TONIC",
+    price: "560000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/SprayGroomingTonic-1-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "SURF TONIC",
+    price: "500000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/SurfTonic-1-768x768.jpg.webp",
+    review:
+      "Just because we’re from Rotterdam doesn’t mean we don’t enjoy a little sea spray in our hair. Reuzel Surf Tonic gives a man’s style instant volume and texture, letting you create a windblown style with a messy, matte finish.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "REUZEL GROOMING TONIC",
+    price: "620000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/GroomingTonic-1-768x768.jpg.webp",
+    review:
+      "Reuzel Grooming Tonic is a tool every man needs on his shelf. You need to add a little extra boost to fine hair, this is just the thing. If you use a blow dryer, it’ll protect your hair. And if you want a natural look, this tonic can stand on its own.",
+  },
+  {
+    category: "HAIR & BODY",
+    name: "REUZEL DAILY CONDITIONER",
+    price: "930000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/DailyConditioner-1-768x768.jpg.webp",
+    review:
+      "Step out of the shower feeling like your scalp’s come right back to life. Reuzel Daily Conditioner does the trick with our own T4 tonic blend of witch hazel, nettle leaf, rosemary and horsetail root extracts.",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL EXTREME HOLD MATTE POMADE",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/ExtremeHoldMattePomade-1-768x768.jpg",
+    review:
+      "Want a messy, dry look that stays in place, all day long? Or do you prefer an ultra, well-groomed look? This is how you get it. Reuzel Extreme Hold Matte Pomade gives you total control, yet its water-based formula washes out with no fuss when the day is done. Magic.",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL FIBER POMADE",
+    price: "610000",
+    url: " https://schorembarbier.nl/wp-content/uploads/2019/12/FiberPomade-1-768x768.jpg",
+    review:
+      "Reuzel Fiber Pomade turns a proper porkchop into a just-out-of-bed style. It’s perfect for those times when a man wants to wear his hair a little loose but still look great.",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL PINK GREASE HEAVY HOLD",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/GreaseHeavyHold-1-768x768.jpg",
+    review:
+      "Pink Reuzel is a wax and oil-based pomade, crafted to create a hold like a brick wall. It’ll give you the power to mold and hold shape all day long.",
+  },
+  {
+    category: "POMADE",
+    name: "REUZEL RED WATER SOLUBLE HIGH SHEEN",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/WaterSolubleHighSheen-1-768x768.jpg",
+    review:
+      "Welcome to the world of shine. Reuzel Red Pomade is one of the products that put us on the map. It comes out of the can like butter. And it combs through the hair, bringing polish and class to any hairstyle.",
+  },
+  {
+    category: "POMADE",
+    name: "REUZEL CLAY MATTE POMADE",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/ClayMattePomade-1-768x768.jpg.webp",
+    review:
+      "Want to get flat, weak hair to stand up? Here’s your answer. Reuzel Clay Matte Pomade will get you volume, texture and height with a dry matte look. It smells amazing and washes out easy.",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL CONCRETE HOLD MATTE POMADE",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2021/05/Concrete-OpenTin.jpg-768x768.webp",
+    review: "",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL BLUE STRONG HOLD WATER SOLUBLE HIGH SHEEN",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/StrongHoldWaterSoluble-1-768x768.jpg",
+    review:
+      "Got the blues? It’s not our fault. Open up the tin. Doesn’t that smell nice? Reuzel blue is perfectly blended with a subtle warm vanilla scent. Use it to create exaggerated pompadours or to tame the most unruly hair.",
+  },
+  {
+    category: "POMADE",
+    name: "REUZEL GREEN GREASE MEDIUM HOLD",
+    price: "610000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/GreaseMediumHold-1-768x768.jpg",
+    review:
+      "Some call it the green grease. And it’s one of our staples. It’s used in barbershop classes worldwide to help stylists construct perfect pompadours and classic contours. And it makes a great cocktail with other products.",
+  },
+  {
+    category: "POMADE",
+    name: "REUZEL GROOMING CREAM",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/GroomingCream-1-768x768.jpg.webp",
+    review:
+      "Reuzel Grooming Cream provides a light hold and works well for all hair types, especially finer hair where added texture or increased definition is desired. Tames hair while leaving a los shine finis",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL MATTE STYLING PASTE",
+    price: "430000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/MatteStylingPaste-1-768x768.jpg.webp",
+    review:
+      "Reuzel Matte Styling Paste has a medium, pliable hold for various lengths of hair, adding texture and increased definition. Leaves hair with a matte finish and is water soluble for easy rinsing.",
+  },
+
+  {
+    category: "POMADE",
+    name: "REUZEL MATTE TEXTURE POWDER",
+    price: "550000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2019/12/MatteTexturePowder-1-768x768.jpg.webp",
+    review:
+      "Reuzel Matte Texture Powder is a weightless, texturing powder that adds volume & thickness to hair and is suitable for all hair types.",
+  },
+  {
+    category: "POMADE",
+    name: "REUZEL CLAY SPRAY",
+    price: "280000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/ClaySpray_1296x-768x768.jpg.webp",
+    review: "",
+  },
+  {
+    category: "SKIN CARE",
+    name: "Clean & Fresh Solid Face Wash Stick",
+    price: "520000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/FaceWash_1296x-768x768.jpg.webp",
+    review:
+      "Sulfate free facial solid cleanser stick intended to use on your dry face and neck.",
+  },
+
+  {
+    category: "SKIN CARE",
+    name: "Hydrating Face Moisturizer",
+    price: "550000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/FaceMoisturizer_1296x-768x768.jpg.webp",
+    review:
+      "Calming, hydrating facial moisturizer that works on all skin types. Helps boost antioxidant activity while brightening and moisturizing the skin.",
+  },
+
+  {
+    category: "SKIN CARE",
+    name: "Intensive Care Eye Cream",
+    price: "580000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2020/12/EyeCream_1296x-768x768.jpg.webp",
+    review:
+      "Quick drying, non-greasy eye cream. RR Intensive Care Eye Cream by REUZEL helps reduce the appearance of dark circles.",
+  },
+  {
+    category: "SKIN CARE",
+    name: "RR Fine Fragrance",
+    price: "560000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2021/08/Fine-Fragrance-768x768.jpg.webp",
+    review:
+      "A light and refreshing fine fragrance from REUZEL® with an uplifting, citrusy scent.",
+  },
+  {
+    category: "SKIN CARE",
+    name: "TAT – Buff Exfoliating Wash",
+    price: "520000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2022/10/Unknown-1-768x768.jpeg.webp",
+    review:
+      "Gentle – Exfoliating – Makes Colors Vibrant. Step #1 of TAT routine. BUFF is a strong and gentle natural cleanser loaded with fruit enzymes to rapidly exfoliate dead skin cells and surface debris from the skin.",
+  },
+  {
+    category: "SKIN CARE",
+    name: "TAT – Hydrabalm",
+    price: "550000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2022/10/Unknown-768x768.jpeg.webp",
+    review:
+      "Moisturizing – Brightening – Protective. Step #2 of your TAT routine. Deeply hydrating balm that helps brighten healed tattoo.",
+  },
+  {
+    category: "SKIN CARE",
+    name: "TAT – Shine Tattoo Spray",
+    price: "580000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2022/10/Unknown-2-768x768.jpeg.webp",
+    review:
+      "Absorbs Fast – Instant Color Pop – Non Greasy. BONUS Step! Add definition and radiance to your healed tattoo with this non greasy, quick dry formula.",
+  },
+  {
+    category: "SKIN CARE",
+    name: "TAT – Vivid Gel",
+    price: "560000",
+    url: "https://schorembarbier.nl/wp-content/uploads/2022/10/Unknown-3-768x768.jpeg.webp",
+    review:
+      "Hydrating – Intensifying – Smoothing. Step #3 of your TAT routine. Hydrating gel that creates a moisture barrier on the skin while quickly enhancing the color of your tattoo.",
+  },
+];
+
+var productDetails = [
+  {
+    details: "-Tăng cường hiệu năng - độ giữ nếp cả ngày mà vẫn cho mái tóc đẹp, một vẻ ngoài tự nhiên. Nó cho một lớp bao bọc hoàn hảo để tăng độ giữ nếp và kết cấu và nó sẽ giúp bảo vệ tóc khỏi sức nóng của sấy khô. - Thấm hút mồ hôi nhẹ: - Tăng độ giữ nếp, độ phồng tự nhiên.   - Phóng đại kết cấu tự nhiên.   - Cung cấp độ khô tự nhiên.   - Không làm nặng tóc, mang lại độ bóng mờ giúp tóc có thêm sức sống.   - Có thể sử dụng trên tóc khô hoặc tóc ẩm vừa dùng khăn khô"
+  },
+  {
+    details: "Giữ nếp cực cao (Heavy/Strong Hold) 10/10 ( 12 - 14 tiếng )     Có độ bóng vừa phải(Medium shine)    Mùi hương: Mùi Vani dịu nhẹ, dễ chịu    Texture(tạo lọn tóc đan xen): Ổn định    Khả năng gội rửa trung bình( 2- 3 lần )."
+  },
+  {
+    details: "Ocean:  mùi hương tươi mát, sảng khoái của tinh dầu gỗ trắc xanh, vị biển và cỏ vetiver   Marble:  mùi hương sảng khoái của nước biển, mùi gỗ và hoa phong lữ  Forest: mùi hương tinh tế thú vị của cam bergamot, gỗ tuyết tùng, cây xô thơm (sage)  Clean Slate: mùi hương tươi mát của cam quýt, cây xô thơm và gỗ đàn hương  Paris: mùi hương tinh tế gỗ tuyết tùng, vỏ cam và một ít xạ hương  Suede: mùi hương cổ điển của da lộn, tuyết tùng và gỗ sồi  Whitewater Rush: mùi hương quyết rũ của cam bergamot và gỗ tuyết tùng trắng. "
+  },
+  {
+    details: "Tăng độ giữ nếp cho mái tóc    Tạo độ phồng tự nhiên / được độ Volume theo ý muốn    Cung cấp độ ẩm cho tóc / giúp mái tóc không bị khô xơ    Bảo vệ tóc khỏi nhiệt độ cao của máy sấy khi sấy tóc    Mùi hương: sự kết hợp của hương táo ngọt dịu & hương bạc hà mát lạnh    *Cực kỳ tốt cho tóc, bảo vệ tóc không bị nhiệt máy sấy làm hư hại và ngược lại giúp cho mái tóc của bạn khỏe và mượt hơn    * Giúp mái tóc khó vào nếp nhất trở nên dễ dàng hơn khi sử dụng sản phẩm"
+  },
+  {
+    details: " Mật ong Manuka Honey tự nhiên từ New Zealand, có chứa dồi dào chất bổ như chất khoáng, vitamin và enzyme giúp lành vết thương hay làn da đang bị bong tróc, nổi vẩy. Chất này còn giúp giữ độ ẩm cao cho làn da.    - Rosepene là một trong những nguyên liệu làm nên tên tuổi của Trilogy với tính năng giúp làm ẩm, làm lành các khuyết điểm trên da, khả năng siêu chống oxy hóa nhằm ngăn chặn các nguy cơ lão hóa da.    - Lavender (tinh dầu oải hương) ngoài mùi hương nổi tiếng, nó còn làm dịu da, se khít lỗ chân lông, cân bằng dầu và khử trùng trên da.    - Coconut oil (dầu dừa) chiết xuất tự nhiên từ phần thịt dừa khô, hoàn hảo trong các việc chống vi khuẩn xâm nhập da cũng như làm dịu da khỏi các tổn thương trên bề mặt. Ngoài ra, với thành phần giúp cung cấp dầu và tinh chất giúp nuôi dưỡng da, dầu dừa còn giúp da giữ nước tốt.    - Evening primrose oil (tinh dầu hoa anh thảo) có hàm lượng gamma-linolenic (GLA) cao, phù hợp cho làn da khô, da xạm, da bị tổn thương cũng như hoàn toàn an toàn cho làn da nhạy cảm. Ngoài ra, GLA là tinh chất phù hợp nhất trong việc nuôi dưỡng và tái tạo lại biểu bì da."
+  },
+];
