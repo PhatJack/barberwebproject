d21d99ebf619e2785100abd116009e5de8b1aff9
PhatJack <jackvuabongro@gmail.com>
2022-12-08 19:36:45 +0700

    add delete function and bug delete button

--- admin/admin.js
+++ admin/admin.js
@@ -1,22 +1,74 @@
-let product_stat = document.querySelector(".product-statistics");
-let menu = document.querySelectorAll(".statistics-menu li");
-let content = document.querySelectorAll(".right-content-bottom");
-let product = document.querySelector(".product-statistics");
-let booking = document.querySelector(".booking-statistics");
-let order = document.querySelector(".order-statistics");
-let account = document.querySelector(".account-statistics");
-const count = 0;
+let product_stat = document.querySelector(".product-statistics")
+let menu = document.querySelectorAll(".statistics-menu li")
+let content = document.querySelectorAll(".right-content-bottom")
+let product = document.querySelector(".product-statistics")
+let booking = document.querySelector(".booking-statistics")
+let order = document.querySelector(".order-statistics")
+let account = document.querySelector(".account-statistics")
+let deleteBtn = document.getElementById("delete")
+let editbtn = document.querySelector("td.status i#edit")
+let tableThing = document.querySelector(".table")
+const count = 0
 menu.forEach((element) => {
     element.addEventListener("click", () => {
         menu.forEach((element) => {
-            element.classList.remove("active");
-        });
-    element.classList.add("active");
-    });
-});
-function changeContent(i){
-    for(let j = 0;j < content.length;j++){
-        content[j].style.display = "none";
+            element.classList.remove("active")
+        })
+        element.classList.add("active")
+    })
+})
+function changeContent(i) {
+    for (let j = 0; j < content.length; j++) {
+        content[j].style.display = "none"
     }
-    content[i].style.display = "block";
+    content[i].style.display = "block"
+}
+function SomeDeleteRowFunction(o) {
+    //no clue what to put here?
+    if (confirm("Bạn có chắc chắn muốn xóa không?")) {
+        var p = o.parentNode.parentNode
+        p.parentNode.removeChild(p)
+    } else {
+        return
+    }
+}
+// let editTable = document.querySelector("tr");
+// let editBtn = document.querySelector("i#edit");
+// editBtn.addEventListener("click",() => {
+//     alert('hello');
+// })
+function editContent(o) {
+    let editTable = document.querySelector("tbody");
+    let html = "";
+    html += `
+        <tr class="edit-table">
+                <td class="input-field id" style="width:10%">
+                    <p>Sửa ID</p>
+                    <input type="number" />
+                </td>
+                <td class="input-field name" style="width:40%">
+                    <p>Sửa tên sản phẩm</p>
+                    <input type="text" />
+                </td>
+                <td class="input-field img" style="width:15%">
+                    <p>Sửa hình ảnh</p>
+                    <input type="file" />
+                </td>
+                <td class="input-field category" style="width:10%">
+                    <p>Sửa loại</p>
+                    <input type="text" />
+                </td>
+                <td class="input-field quantity" style="width:10%">
+                    <p>Sửa số lượng</p>
+                    <input type="number" />
+                </td>
+                <td class="input-field btn" style="width:15%">
+                    <input type="submit" class="dec-btn" value="Hủy"/>
+                    <input type="submit" class="acp-btn" value="Đồng ý"/>
+                </td>
+        </tr>
+    `
+    editTable.innerHTML += html
+    let temp = document.querySelector(".edit-table");
+    temp.classList.toggle("active");
 }
--- admin/getDataForProduct.js
+++ admin/getDataForProduct.js
@@ -1,13 +1,21 @@
-let table = document.querySelector(".table");
+let table = document.querySelector(".product-table")
+let tableBooking = document.querySelector(".booking")
+let tableOrder = document.querySelector(".order")
+let tableAccount = document.querySelector(".account")
 // console.log("123");
-const api = "../product/O.JSON";
+const api = "../product/O.JSON"
+const apiBooking = "../ApiJson/booking.json"
+const apiOrder = "../ApiJson/order.json"
+const apiAccount = "../login/json-server/data.json"
 async function getData() {
-    const response = await fetch(api);
-    const data = await response.json();
+    const response = await fetch(api)
+    const data = await response.json()
     // console.log(data);
     data.forEach((element) => {
-    table.innerHTML += `
+        table.innerHTML += `
         <tbody>
+            <tr>
+            <td>${element.id}</td>
             <td>${element.name}</td>
             <td>
                 <div class="product-img">
@@ -17,12 +25,72 @@ async function getData() {
             <td>${element.category}</td>
             <td>${element.quantity}</td>
             <td class="status">
-                <i class="fa-solid fa-pen-to-square"></i>
-                <i class="fa-solid fa-trash"></i>
+                <i class="fa-solid fa-pen-to-square" id="edit" onclick="editContent(this)"></i>
+                <i class="fa-solid fa-trash" id="delete" onclick="SomeDeleteRowFunction(this)"></i>
             </td>
+            </tr>
         </tbody>
-        `;
-    });
+        `
+    })
 }
-
-getData();
+async function getDataBooking() {
+    const response = await fetch(apiBooking)
+    const data = await response.json()
+    data.forEach((element) => {
+        tableBooking.innerHTML += `
+            <tbody>
+            <td>${element.name}</td>
+            <td>${element.phone}</td>
+            <td>${element.numberOfCustomer}</td>
+            <td>Chi Nhánh ${element.branch}</td>
+            <td>${element.date}</td>
+            <td>${element.time}</td>
+            <td class="status">
+                <i class="fa-solid fa-pen-to-square" id="edit"></i>
+                <i class="fa-solid fa-trash" id="delete" onclick="SomeDeleteRowFunction(this)"></i>
+            </td>
+    </tbody>
+        `
+    })
+}
+async function getDataOrder() {
+    var temp
+    const response = await fetch(apiOrder)
+    const data = await response.json()
+    data.forEach((element) => {
+        tableOrder.innerHTML += `
+            <tbody>
+            <td>${element.username}</td>
+            <td>${element.phone}</td>
+            <td>${element.product[element.product.length - 1].id}</td>
+            <td>${element.product[element.product.length - 1].count}</td>
+            <td>${element.address}</td>
+            <td class="status">
+                <i class="fa-solid fa-pen-to-square" id="edit"></i>
+                <i class="fa-solid fa-trash" id="delete" onclick="SomeDeleteRowFunction(this)"></i>
+            </td>
+    </tbody>
+        `
+    })
+}
+async function getDataAccount() {
+    const response = await fetch(apiAccount)
+    const data = await response.json()
+    data.forEach((element) => {
+        tableAccount.innerHTML += `
+            <tbody>
+            <td>${element.username}</td>
+            <td>${element.password}</td>
+            <td>${element.email}</td>
+            <td class="status">
+                <i class="fa-solid fa-pen-to-square" id="edit" onclick="editContent()"></i>
+                <i class="fa-solid fa-trash" id="delete" onclick="SomeDeleteRowFunction(this)"></i>
+            </td>
+    </tbody>
+        `
+    })
+}
+getData()
+getDataBooking()
+getDataOrder()
+getDataAccount()
